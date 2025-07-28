f458afe9ac0cb0c7b9991f3d3f876263278d57fe
sebastian097 <sebastian.landazuri@polivoz.com>
2025-07-27 20:40:08 -0500

    initial project

---
+++ seek/src/main/java/com/seek/SistemaDeGestionDeCandidatosEnProcesoDeSeleccionYContratacionApplication.java
@@ -0,0 +1,13 @@
+package com.seek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionDeCandidatosEnProcesoDeSeleccionYContratacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionDeCandidatosEnProcesoDeSeleccionYContratacionApplication.class, args);
+	}
+
+}
